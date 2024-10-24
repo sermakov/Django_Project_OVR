c4a552b51c39b1b88566e6054da8274b236b2bc6
Sergey Ermakov <seermakov355@yandex.ru>
2024-10-24 20:51:09 +0300

    Коррекция html-шаблона home

--- event_manager/event_manager/urls.py
+++ event_manager/event_manager/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls', namespace='events')),
+    path('about/', include('about.urls', namespace='about')),
+    path('contact/', include('contact.urls', namespace='contact')),
 ]
