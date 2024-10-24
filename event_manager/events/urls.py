193742e4c62637bb25d13143df98055c2133de76
Sergey Ermakov <seermakov355@yandex.ru>
2024-10-24 20:48:33 +0300

    Созданы представления и шаблоны, добавлено описание задания

--- event_manager/events/urls.py
+++ event_manager/events/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'events'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('events/', views.event_list, name='event_list'),
+    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
--- event_manager/events/views.py
+++ event_manager/events/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'events/home.html')
+
+def event_list(request):
+    # В будущем здесь будет вывод списка мероприятий
+    return render(request, 'events/event_list.html')
+
+def event_detail(request, event_id):
+    # В будущем здесь будет вывод деталей мероприятия
+    return render(request, 'events/event_detail.html', {'event_id': event_id})
